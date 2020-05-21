c4c2cd1d6fdf3e8b81895d1600609b6297f8cca4
rocio <contactolda@gmail.com>
2020-05-20 22:15:18 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+   <h1>Muestra de Series</h1>
+    <series></series>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Series from './components/Series.vue';
 
 export default {
   name: 'App',
+ data(){
+   return {
+     series :[]
+   }
+ },
+  mounted(){
+    axios.get('http://api.tvmaze.com/shows')
+    .then(response => {
+      console.log(response.data);
+      this.series = response.data;
+    })
+    .catch (error => console.log(error))
+  },
   components: {
-    HelloWorld
+    Series
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
